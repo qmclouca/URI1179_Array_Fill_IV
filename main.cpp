ffdae0251fbca71322c4ad2eb5855a6ed0f124a8
qmclouca <qmclouca@gmail.com>
2021-02-05 20:38:29 -0300

    Uma saída errada. Os quintos elementos de cada vetor estão retornando 0;

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
-int number = 0, par[5] = {0}, impar[5] = {0}, countPar = 0, countImpar = 0, contTotalPrintedPar = 0, contTotalPrintedImpar = 0;
+int number = 0, par[5], impar[5];
+int countImpar = 0, contTotalPrintedPar = 0,  countPar = 0, contTotalPrintedImpar = 0;
 bool parFull = false, imparFull = false;
-int main() {
 
+int main() {
     for (int aux = 0; aux < 15; aux++){
         std::cin >> number;
         if (number%2 == 0){
@@ -11,14 +12,14 @@ int main() {
             if (parFull){
                 parFull = false;
                 for (int auy=0; auy<5; auy++){
-                    std::cout << "par["<<auy<<"] = "<< par[auy]<<std::endl;
+                    std::cout << "par["<< auy << "] = " << par[auy] << std::endl;
                     contTotalPrintedPar--;
                     countPar = 0;
                 }
             } else {
                 par[countPar] = number;
                 countPar++;
-                if (countPar == 5){
+                if (countPar == 4){
                     parFull = true;
                 }
             }
@@ -27,28 +28,28 @@ int main() {
             if (imparFull){
                 imparFull = false;
                 for (int auy=0; auy<5; auy++){
-                    std::cout << "impar["<<auy<<"] = "<< impar[auy]<<std::endl;
+                    std::cout << "impar[" << auy << "] = " << impar[auy] << std::endl;
                     contTotalPrintedImpar--;
                     countImpar = 0;
                 }
             } else {
                 impar[countImpar] = number;
                 countImpar++;
-                if (countImpar == 5){
+                if (countImpar == 4){
                     imparFull = true;
                 }
             }
         }
     }
     if (contTotalPrintedImpar!=0){
-        for (int auy=0; contTotalPrintedImpar>=0; auy++,contTotalPrintedImpar--){
+        for (int auy=0; contTotalPrintedImpar>0; auy++,contTotalPrintedImpar--){
             if (impar[auy]!=0) {
                 std::cout << "impar[" << auy << "] = " << impar[auy] << std::endl;
             }
         }
     }
     if (contTotalPrintedPar!=0){
-        for (int auy=0; contTotalPrintedPar>=0; auy++, contTotalPrintedPar--){
+        for (int auy=0; contTotalPrintedPar>0; auy++, contTotalPrintedPar--){
             if (par[auy]!=0) {
                 std::cout << "par[" << auy << "] = " << par[auy] << std::endl;
             }
